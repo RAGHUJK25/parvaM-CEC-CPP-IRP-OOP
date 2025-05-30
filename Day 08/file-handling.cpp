373181ac4796f0d7548523ea6d6ed178fec6022b
RAGHUJK25 <raghu8310718116.com>
2025-05-30 16:07:02 +0530

    Pointer-to-array, pointer-to-objecct , file ctreating , file reading and writing

--- Day 08/file-handling.cpp
+++ Day 08/file-handling.cpp
@@ -4,6 +4,11 @@
 using namespace std;
 
 int main(){
+    // fstream header file provides 3 classes for file handling:
+    // fastreambase
+    // ifstream(It is Derived by fstreambase class)- read from the file
+    // ofstream(It is Derived by fstreambase class)- write to the file
+    // writing in the file
     ofstream out("sample.txt"); // Create an output file stream to write to "sample.txt"
     string name;
     int Age;
